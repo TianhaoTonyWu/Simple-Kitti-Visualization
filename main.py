c7f28193753b9890bd2f862e64abd6f49aad9286
Tianhao Wu <90681352+TianhaoTonyWu@users.noreply.github.com>
2022-07-30 16:57:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,42 +4,43 @@ from views import *
 import cv2
 import sys
 
-    # define frame name and get path for each file
-    frame = sys.argv[1]
-    print("Visualizing frame "+frame)
-    img_path = 'data/' + frame + '.png'
-    pts_path = 'data/' + frame + '.bin'
-    calib_path = 'data/' + frame + '_calib.txt'
-    label_path = 'data/' + frame + '_label.txt'
 
-    # define output path for image visualisation and create image
-    output_path = "output/vis_image"+sys.argv[1]+".png"
-    img = vis_image(
-        output_path,
-        img_path,
-        pts_path,
-        calib_path,
-        label_path,
-        lidar=False,
-        boxes_2d=False,
-        boxes_3d=True
-    )
+# define frame name and get path for each file
+frame = sys.argv[1]
+print("Visualizing frame "+frame)
+img_path = 'data/' + frame + '.png'
+pts_path = 'data/' + frame + '.bin'
+calib_path = 'data/' + frame + '_calib.txt'
+label_path = 'data/' + frame + '_label.txt'
 
-    cv2.imwrite(output_path, img)
-    print("Saved img:", output_path)
+# define output path for image visualisation and create image
+output_path = "output/vis_image"+sys.argv[1]+".png"
+img = vis_image(
+    output_path,
+    img_path,
+    pts_path,
+    calib_path,
+    label_path,
+    lidar=False,
+    boxes_2d=False,
+    boxes_3d=True
+)
 
-    # define output path for points visualisation and create image
-    output_path = "output/vis_points"+sys.argv[1]+".png"
-    img = vis_points(
-        output_path,
-        pts_path,
-        label_path,
-        calib_path,
-        boxes_3d=True,
-        view_dict=TPV # TPV(third person view), FPV(first person view), BEV(birds eye view)
-    )
+cv2.imwrite(output_path, img)
+print("Saved img:", output_path)
 
-    cv2.imwrite(output_path, img)
-    print("Saved img:", output_path)
+# define output path for points visualisation and create image
+output_path = "output/vis_points"+sys.argv[1]+".png"
+img = vis_points(
+    output_path,
+    pts_path,
+    label_path,
+    calib_path,
+    boxes_3d=True,
+    view_dict=TPV # TPV(third person view), FPV(first person view), BEV(birds eye view)
+)
+
+cv2.imwrite(output_path, img)
+print("Saved img:", output_path)
 
 
