a8fac61393cada7e27037f2055addbcb0c308400
Tianhao Wu <90681352+TianhaoTonyWu@users.noreply.github.com>
2022-08-01 13:04:24 -0400

    Update vis_image.py

--- vis_image.py
+++ vis_image.py
@@ -41,7 +41,7 @@ def vis_image(
                 random.shuffle(lidar_pallet)
             color = lidar_pallet.pop()
             print(color)
-            if(labels[label_index] != 'Dontcare') cv2.putText(img, labels[label_index], (label_pos, 30), font, 1, color, 2, cv2.LINE_AA)
+            if labels[label_index] != 'Dontcare': cv2.putText(img, labels[label_index], (label_pos, 30), font, 1, color, 2, cv2.LINE_AA)
             label_index += 1
             label_pos += 70
             img = repro_box(img, box, intrinsics, color)
